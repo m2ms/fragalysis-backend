d3ad07cb9730df2210a17de1efac8972920635b4
Tibor Postek <roman.pis@compote.eu>
2020-03-13 08:50:47 +0100

    #1 with upgrading of fragalysis

--- launch-stack.sh
+++ launch-stack.sh
@@ -18,6 +18,9 @@ else:
 "
 printf "$script" | python manage.py shell
 
+echo "upgrade fragalysis"
+pip install --upgrade fragalysis
+
 # Prepare log files and start outputting logs to stdout
 touch /srv/logs/gunicorn.log
 touch /srv/logs/access.log
